1627d736af5e48cc912f7cd4b2eba972158088ce
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-19 19:34:58 +0400

    database updated -python manage.py makemigrations

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+# The Category class is a model in Django with a name attribute of type CharField.
+class Category(models.Model):
+    name= models.CharField(max_length=255)
