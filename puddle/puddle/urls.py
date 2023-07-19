c09defaa98504c89791215ccc8011aaf54dca155
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-19 14:21:44 +0400

    contact url added

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
+# The code `from core.views import index` is importing the `index` function from the `views` module in
+# the `core` package.
+# The line `from core.views import index, contact` is importing the `index` and `contact` functions
+# from the `views` module in the `core` package. This allows you to use these functions in your code.
+from core.views import index,contact
 
 
 urlpatterns = [
+    
     path('',index, name='index'),
+    path('contact/',contact,name='contact'),
     path('admin/', admin.site.urls),
 ]
