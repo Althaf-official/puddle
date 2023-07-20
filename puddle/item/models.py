cc1f33e2fc04406e79c700f6cfdf5f19fc60299a
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-20 16:23:58 +0400

    showing add catogoryies name individually

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -4,3 +4,9 @@ from django.db import models
 # The Category class is a model in Django with a name attribute of type CharField.
 class Category(models.Model):
     name= models.CharField(max_length=255)
+
+    class meta:
+        verbose_name_plural='Categories'
+
+    def __str__(self) -> str:
+        return self.name
