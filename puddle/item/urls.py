40275ba95d60b4ebdc87b801fc94869cca61264b
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-25 09:48:32 +0400

    Detail page of the item . showed the pic

---
+++ puddle/item/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+
+from . import views
+
+app_name='item'
+
+urlpatterns =[
+    path('<int:pk>/',views.detail,name='detail'),
+]
+
+
+
+"""
+urlpatterns: This is a list that stores all the URL patterns for your web application.
+
+path(): It's a function provided by Django that allows you to define a URL pattern. It takes three arguments:
+
+The first argument is the URL pattern, which can contain special placeholders, such as <int:pk>. The pk here is a variable name, 
+and <int> indicates that the value passed for this part of the URL should be an integer. 
+This is called a path converter and allows you to capture values from the URL and pass them as arguments to the view function.
+
+The second argument is the view function that will be called when a user accesses a URL that matches the defined pattern. 
+In this case, the view function is views.detail. When a user accesses a URL like yourdomain.com/123, 
+the detail function in the views.py file will be executed.
+
+The third argument is the optional name parameter, which provides a unique name for the URL pattern. 
+This name can be used to reverse the URL later in the code. For example, in templates or views, 
+you can refer to the URL pattern by its name instead of hardcoding the URL.
+
+In this specific example, the URL pattern is '<int:pk>', which means it expects an integer value in the URL. 
+When a user visits a URL like yourdomain.com/123, 
+Django will capture the integer value 123 and pass it as an argument pk to the detail view function.
+"""
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 # The code `from core.views import index` is importing the `index` function from the `views` module in
 # the `core` package.
@@ -13,6 +13,7 @@ from core.views import index,contact
 urlpatterns = [
     
     path('',index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/',contact,name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
