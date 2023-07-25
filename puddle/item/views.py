634355d3481e32810c6a5870293a75eb6ca0c55e
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-25 08:59:27 +0400

    Detail page of the Item -creted html file

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render,get_object_or_404
 
 from .models import Item
 
-def detail(request, pk):
-    item=get_object_or_404(Item, pk=pk)
+def detail(request, pk):#pk for primary key
+    item=get_object_or_404(Item, pk=pk)#the right pk is from we get the url
 
     return render(request, 'item/detail.html',{
         'item': item
