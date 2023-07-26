b8afe9696d93fe7babc8330988f3149ca462ea40
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-26 06:50:14 +0400

    Implemented Related Items

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -4,9 +4,11 @@ from .models import Item
 
 def detail(request, pk):#pk for primary key
     item=get_object_or_404(Item, pk=pk)#the right pk is from we get the url
+    related_items =Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
 
     return render(request, 'item/detail.html',{
-        'item': item
+        'item': item,
+        'related_items': related_items,
     })
 
 
