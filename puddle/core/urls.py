39db0251047b9f78b4de80f0c941b9278e122e28
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-26 15:17:36 +0400

    login page authentication - auth_views.LoginView.as_view() is used. It is a built-in Django class-based view that handles the login functionality. This view provides the necessary logic to display the login form, process login requests, and handle authentication.

--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', views.index,name='index'),
     path('contact/',views.contact,name='contact'),
     path('signup/',views.signup, name='signup'),
+    path('login/',auth_views.LoginView.as_view(),name='login')
 ]
