98e6d00c71ad67793e53db56ae15fe15614a7b7c
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-26 14:18:11 +0400

    Signup page - username,email,password

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -7,3 +7,24 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields =('username','email','password1','password2')
+
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Enter Your Username',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Enter Your email',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Your Password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Repeat your password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
