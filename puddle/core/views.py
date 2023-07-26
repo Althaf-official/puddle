984b9ace68f23f97479ac620bf4e5788f4b5ec8f
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-26 07:19:20 +0400

    created function for signup

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from item.models import Category,Item
 
+from .forms import SignupForm
+
 # Create your views here.
 def index(request):
     items =Item.objects.filter(is_sold=False)[0:6]
@@ -15,3 +17,11 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    form =SignupForm()
+
+    return render(request, 'core/signup.html',{
+        'form':form
+    })
