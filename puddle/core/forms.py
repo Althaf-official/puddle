7d68f896e2ad2c64178fb9a2040ac7f042af02b0
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-26 15:09:42 +0400

    creates a custom login form that inherits from Django's AuthenticationForm.

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -3,6 +3,18 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Enter Your Username',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Your Password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+
+
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
