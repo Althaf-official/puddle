998697a0bdc04f2cff22ce646074a84ac5602d10
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-02 20:40:06 +0400

    added some data

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,34 +1,46 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import NewItemForm
 from .models import Item
 
-def detail(request, pk):#pk for primary key
-    item=get_object_or_404(Item, pk=pk)#the right pk is from we get the url
-    related_items =Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
 
-    return render(request, 'item/detail.html',{
-        'item': item,
-        'related_items': related_items,
-    })
+def detail(request, pk):  # pk for primary key
+    item = get_object_or_404(Item, pk=pk)  # the right pk is from we get the url
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(
+        pk=pk
+    )[0:3]
+
+    return render(
+        request,
+        "item/detail.html",
+        {
+            "item": item,
+            "related_items": related_items,
+        },
+    )
+
 
 @login_required
 def new(request):
-
     #!handles the processing of form data submitted via a POST request,associates the item with the currently logged-in user, and saves the item to the database.
     if request.method == "POST":
-        form = NewItemForm(request.POST,request.FILES)
+        form = NewItemForm(request.POST, request.FILES)
 
         if form.is_valid():
-            item=form.save(commit=False)
-            item.created_by =request.user
+            item = form.save(commit=False)
+            item.created_by = request.user
             item.save()
 
+            return redirect("item:detail", pk=item.id)
+    else:
+        form = NewItemForm()
 
-    form = NewItemForm()
-
-    return render(request, "item/form.html", {
-        "form": form,
-        'title': 'New Item',
-        })
+    return render(
+        request,
+        "item/form.html",
+        {
+            "form": form,
+            "title": "New Item",
+        },
+    )
