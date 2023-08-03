5bbf8fc3b81883d9983e0c5a6811fb3305262fad
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-03 07:13:20 +0400

    SEARCHING -the items view function fetches a list of unsold items from the database and passes that list as context data to an HTML template named "item/items.html". The template can then use this data to display information about the unsold items, allowing users to view a list of available items for purchase.

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm,EditItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
+
+
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+
+    return render(request, "item/items.html", {"items": items,})
+
+
 
 
 def detail(request, pk):  # pk for primary key
@@ -36,17 +44,22 @@ def new(request):
     else:
         form = NewItemForm()
 
-    return render(request, 'item/form.html',{
-            'form': form,
-            'title': 'New Item',
-        })
+    return render(
+        request,
+        "item/form.html",
+        {
+            "form": form,
+            "title": "New Item",
+        },
+    )
+
 
 @login_required
 def edit(request, pk):
-    item= get_object_or_404(Item,pk=pk,created_by=request.user)
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
 
     if request.method == "POST":
-        form = EditItemForm(request.POST, request.FILES,instance=item)
+        form = EditItemForm(request.POST, request.FILES, instance=item)
 
         if form.is_valid():
             form.save()
@@ -54,15 +67,19 @@ def edit(request, pk):
     else:
         form = EditItemForm(instance=item)
 
-    return render(request, 'item/form.html',{
-            'form': form,
-            'title': 'Edit Item',
-        })
+    return render(
+        request,
+        "item/form.html",
+        {
+            "form": form,
+            "title": "Edit Item",
+        },
+    )
 
 
 @login_required
-def delete(request,pk):
-    item= get_object_or_404(Item,pk=pk,created_by=request.user)
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
     item.delete()
 
     return redirect("dashboard:index")
