7930c3dded4d53fdf508bc1447ec532a0030b179
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-03 05:49:05 +0400

    Dashboard - created a  function | Delete items by the user created

--- puddle/dashboard/views.py
+++ puddle/dashboard/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 from item.models import Item
 
@@ -16,3 +16,4 @@ def index(request):
         },
     )
     #!function called index that only allows authenticated users to access the page. It fetches a list of items created by the currently logged-in user and passes that list as context data to an HTML template named "dashboard/index.html". The template can then render the list of items for display on the web page
+
