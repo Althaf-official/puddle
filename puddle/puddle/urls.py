bb11248410330a967a94a2640e3040e72b530714
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-03 05:34:42 +0400

    Dashboard - added to the main url

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard/',include("dashboard.urls")),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
