d203c164a6b1436022af6a07079a5e6b75eb1d76
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-03 05:51:05 +0400

    Dashboard - Import delete to the Url page

--- puddle/item/urls.py
+++ puddle/item/urls.py
@@ -7,6 +7,7 @@ app_name='item'
 urlpatterns =[
     path("new/", views.new, name="new"),
     path('<int:pk>/',views.detail,name='detail'),
+    path('<int:pk>/delete/',views.delete,name='delete'),
 ]
 
 
